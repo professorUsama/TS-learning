eace6a6ea8cb8ad81909dec66bc5d26bc3817c85
Professor Usama <149573452+usamaamjid@users.noreply.github.com>
2023-12-09 00:44:19 +0500

    update

--- index.ts
+++ index.ts
@@ -37,12 +37,115 @@ function getSomeThing(){
 }
 
 someThing = getSomeThing()
-console.log(someThing);
+// console.log(someThing);
 
 // The any type is useful when you don’t want to write out a long type just to convince TypeScript that a particular line of code is okay.
 // You usually want to avoid this, though, because any isn’t type-checked. Use the compiler flag noImplicitAny to flag any implicit any as an error.
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// functions in TS
+
+// When you declare a function, you can add type annotations after each parameter to declare what types 
+// of parameters the function accepts. Parameter type annotations go after the parameter name:
+function adder(n1: number, n2: number){
+    return n1 + n2;
+}
+
+// When a parameter has a type annotation, arguments to that function will be checked:
+// Would be a runtime error if executed!
+// console.log(add("professor", "usama")); type will be check at a runtime
+
+console.log(adder(10, 5));
+
+
+// Return Type Annotations
+// the function when return someting like string, boolean, object/arrays, number or nothing, 
+// then we need to add return type annotations and it is a best practice
+
+// as we can see that this function return something. now we can identify what is the type of return value
+// the return type of return value is string; so type annotation will be string
+function greet(msg: string): string{
+    return msg;
+}
+
+const msg = greet("Good Morning!") // here const msg automatically set type string. its type inference.
+// console.log(msg);
+
+// return type will be boolean
+function isPositive(number: number): boolean{
+    return number > 0;
+}
+
+// console.log(isPositive(-1));
+
+// promises function return type
+
+// If you want to annotate the return type of a function which returns a promise, you should use the Promise type:
+
+async function getFvtName(): Promise<string>{
+    return "Professor";
+}
+
+const fvtName = getFvtName(); // when we hove on function name, then we can see return type
+// console.log(fvtName);
+
+// Anonymous Functions
+
+// contextual typing
+// Anonymous functions are a little bit different from function declarations.
+// Here’s an example:
+
+const programmingLanguagesName = ['JavaScript', 'TypeScript', 'Python'];
+// Contextual typing for function - parameter s inferred to have type string
+programmingLanguagesName.forEach(function(nameInString){
+    // console.log(nameInString.toUpperCase());
+});
+
+// Contextual typing also applies to arrow functions
+programmingLanguagesName.forEach((names) =>{
+    // console.log(names.toLowerCase());
+});
+
+// Even though the parameter 'nameInString' and 'names' didn’t have a type annotation, TypeScript used the types of 
+// the forEach function, along with the inferred type of the array, to determine the type s will have.
+// This process is called contextual typing because the context that the function occurred within informs what type 
+// it should have.
+
+// Object Types
+// here’s a function that takes a point-like object:
+function mergeName(name: {firstName: string; lastName: string}){
+    console.log(`${name.firstName} ${name.lastName}`);
+}
+mergeName({firstName: "usama", lastName: "amjid"});
+
+// Here, we annotated the parameter with a type with two properties name and lastName which are both of type number. 
+// You can use , or ; to separate the properties, and the last separator is optional either way.
+// The type part of each property is also optional. If you don’t specify a type, it will be assumed to be any.
+// and its not a good practice to use any
+
+// Optional Properties
+
+// Object types can also specify that some or all of their properties are optional. To do this, add a ? 
+// after the property name:
+
+function printName(obj: {first: string; last?: string}){
+    if(!obj.last) {
+        console.log(`${obj.first}`)
+    } else {
+        console.log(`${obj.first} ${obj.last}`)
+    }
+}
+
+// both accept
+// printName({first: "professor"});
+printName({first: "professor", last: "usama"});
+// In JavaScript, if you access a property that doesn’t exist, you’ll get the value undefined rather than a runtime error. 
+// Because of this, when you read from an optional property, you’ll have to check for undefined before using it
+
+
+
 
 
